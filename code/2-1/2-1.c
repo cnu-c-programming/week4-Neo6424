aadf9bc5277be8821c5e7f503e361f2d9eef8b0c
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-03-27 09:13:27 +0900

    Update 2-1.c

---
+++ code/2-1/2-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    const char* date = "2026-03-24";
+    char buffer[64];
+    int year, month, day;
+  
+    sscanf(date, "%d-%d-%d", &year, &month, &day);
+    sprintf(buffer, "%02d/%02d/%d", day, month, year);
+
+    printf("%s\n", buffer);
+    return 0;
+}
